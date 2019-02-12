8a0998c4cbfea0ca7c4b23f0966c6fb91b9cc7f2
ksham001 <ksham001@ucr.edu>
2019-02-12 08:48:48 +0000

    finished manhattan dist. need to finish node expansion and main fucntion

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include <queue>
 #include <vector>
-#include<sstream>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
 //H_n huerisitic functions for A*
-int Misplaced_Tile_H(vector<vector <int> > puzzle_state){
+int Misplaced_Tile(vector<vector <int> > puzzle_state){
    int H_n = 0;//H(n) value to be plugged into f(n) = H(n) + g(n)
    for(int i = 0; i < puzzle_state.size(); i++){
        for(int j = 0; j < puzzle_state.at(i).size(); j++){
@@ -19,11 +20,57 @@ int Misplaced_Tile_H(vector<vector <int> > puzzle_state){
    return H_n; 
 };
 
+void actual_position(int& val, int& x, int& y)
+{
+	switch (val) {
+	case 1:
+		x = 0;
+		y = 0;
+		break;
+	case 2:
+		x = 0;
+		y = 1;
+		break;
+	case 3:
+		x = 0;
+		y = 2;
+		break;
+	case 4:
+		x = 1;
+		y = 0;
+		break;
+	case 5:
+		x = 1;
+		y = 1;
+		break;
+	case 6:
+		x = 1;
+		y = 2;
+		break;
+	case 7:
+		x = 2;
+		y = 0;
+		break;
+	case 8:
+		x = 2;
+		y = 1;
+		break;
+	}
+};
+
 int Manhattan_Distance(vector<vector <int> > puzzle_state){
     int H_n = 0;
+    int act_x, act_y;
+    
     for(int i = 0; i < puzzle_state.size(); i++){
        for(int j = 0; j < puzzle_state.at(i).size(); j++){
-           H_n += 0;
+           if(puzzle_state.at(i).at(j) != 0){
+              
+               //H_n += abs(   ((puzzle_state.at(i).at(j) - 1) / (puzzle_state.at(i).size())) - i   )  + abs(   ((puzzle_state.at(i).at(j) - 1) % (puzzle_state.at(i).size())) - j   );
+               //H_n +=   abs(((puzzle_state.at(i).at(j) - 1) / (puzzle_state.at(i).size())) - i)  +   abs(((puzzle_state.at(i).at(j) - 1) % (puzzle_state.at(i).size())) - j);
+                actual_position(puzzle_state.at(i).at(j),act_x,act_y);
+                H_n += abs(i-act_x) +abs(j-act_y);
+           }
        }
    }
     return H_n;
@@ -58,15 +105,23 @@ bool Compare_Puzzles(Node a, Node b){
     return puzzles_are_the_same;    
 };
 
-void Generate_Next_Nodes(vector< vector<int> > &node_queue){
+void Generate_Next_Nodes(vector< vector<int> > &node_queue, int puzzle_heuristic){
     
     return;
 };
 
-
+void Print_Node(Node a){
+    for(int i = 0; i < a.puzzle_box.size(); i++){
+        for(int j = 0; j < a.puzzle_box.at(i).size(); j++){
+            cout<<a.puzzle_box.at(i).at(j)<<" ";
+        }
+        cout<<endl;
+    }    
+    return;
+}
 
 int main(){
-    
+   
     string string_parse;
     int int_parse;
     int puzzle_form; //should be either 1 or 2 denoting defualt puzzle or their own puzzle
@@ -114,29 +169,31 @@ int main(){
     getline(cin,string_parse);
     stringstream(string_parse) >> puzzle_form; 
    
-    
-    cout<<"\tEnter your puzzle, use a zero to represent the blank\n";
-    cout<<"\tEnter the first row, use space or tabs between numbers\t\t";
-    //TODO: put this iterative sequence in a loop to make it the puzzle is expandable in size
-    getline(cin,string_parse);
-    stringstream ss1(string_parse); //TODO: figure out how to use 1 stringstream instance
-    
-    while(ss1>>int_parse){
-        input_puzzle.at(0).push_back(int_parse);
-    }
-    
-    //TODO: grab first row
-    cout<<"\tEnter the second row, use space or tabs between numbers\t\t";
-    getline(cin,string_parse);
-    stringstream ss(string_parse); 
-    while(ss>>int_parse){
-        input_puzzle.at(1).push_back(int_parse);
-    }
-    cout<<"\tEnter the third row, use space or tabs between numbers\t\t";
-    getline(cin,string_parse);
-   stringstream ss2(string_parse); 
-    while(ss2>>int_parse){
-        input_puzzle.at(2).push_back(int_parse);
+    if(puzzle_form == 2){
+        
+        cout<<"\tEnter your puzzle, use a zero to represent the blank\n";
+        
+        cout<<"\tEnter the first row, use space or tabs between numbers\t\t";
+        //TODO: put this iterative sequence in a loop to make it the puzzle is expandable in size
+        getline(cin,string_parse);
+        stringstream ss1(string_parse); //TODO: figure out how to use 1 stringstream instance
+        while(ss1>>int_parse){
+            input_puzzle.at(0).push_back(int_parse);
+        }
+        
+        cout<<"\tEnter the second row, use space or tabs between numbers\t\t";
+        getline(cin,string_parse);
+        stringstream ss(string_parse); 
+        while(ss>>int_parse){
+            input_puzzle.at(1).push_back(int_parse);
+        }
+        
+        cout<<"\tEnter the third row, use space or tabs between numbers\t\t";
+        getline(cin,string_parse);
+        stringstream ss2(string_parse); 
+        while(ss2>>int_parse){
+            input_puzzle.at(2).push_back(int_parse);
+        }
     }
     
     cout<<"\n\tEnter your choice of algorithm"; 
@@ -146,6 +203,28 @@ int main(){
     getline(cin,string_parse);
     stringstream(string_parse) >> puzzle_heuristic; 
     
+    cout<<"\nExpanding State\n\n";
+    Node given_puzzle = Node(default_puzzle, 0 ,0);
+    
+
+    if(puzzle_heuristic == 1){
+        given_puzzle = Node(default_puzzle,0,0);
+    }else if(puzzle_heuristic == 2){
+        given_puzzle = Node(input_puzzle,0,Misplaced_Tile(input_puzzle));
+    }else if(puzzle_heuristic == 3){
+        given_puzzle =Node(input_puzzle,0,Manhattan_Distance(input_puzzle));
+    }else{
+        cout<<"ERROR: PLEASE ENTER A VALID HEURISTIC. ACCPETABLE INPUT ARE 1 , 2, and 3."<<endl;
+    }
+    
+    Print_Node(given_puzzle);
+    node_queue.push(given_puzzle);
+    cout<<"done";
+    //while(!node_queue.empty()){
+        
+    //}
+    
+    /*
     cout<<"puzzle input"<<endl;
     for(int i = 0; i <input_puzzle.size(); i++){
         for(int j = 0; j < input_puzzle.at(0).size(); j++){
@@ -161,6 +240,10 @@ int main(){
         }
         cout<<endl;
     }
+    
+    
+    cout<<"manhattan distance of default puzzle: "<<Manhattan_Distance(default_puzzle)<<endl;
+    */
 //TODO: print expanded manhattan 
     return 0;
 }
